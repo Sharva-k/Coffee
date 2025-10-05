24aaf3fa3f7cc94822b7e6fe0a44eb2fd55a5e91
Sharva-k <kalesharva@gmail.com>
2025-10-05 19:41:32 +0530

    Coffee

---
+++ main.js
@@ -0,0 +1,26 @@
+let svg = document.querySelector("svg");
+let path = svg.querySelector("path");
+
+const pathLength = path.getTotalLength();
+
+gsap.set(path, {
+  strokeDasharray: pathLength,
+});
+
+gsap.fromTo(
+  path,
+  {
+    strokeDashoffset: pathLength,
+  },
+  {
+    strokeDashoffset: 0,
+    dusration: 10,
+    ease: "none",
+    scrollTrigger: {
+      trigger: ".svg-container",
+      start: "top top",
+      end: "bottom bottom ",
+      scrub: 1,
+    },
+  }
+);
